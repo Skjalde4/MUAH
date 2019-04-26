b0394523ba739f261e73c38d411c6e63d25a2f5b
Helena Graff <helenagraff@hotmail.com>
2019-04-26 18:12:36 +0200

    version 1.8

---
+++ MUAH/ViewModel/CustomerViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using MUAH.Annotations;
+using MUAH.Model;
+
+namespace MUAH.ViewModel
+{
+    class CustomerViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<Customer> _customers;
+        public CustomerSingleton CustomerSingleton { get; set; }
+
+        public CustomerViewModel()
+        {
+            _customers = new ObservableCollection<Customer>();
+            CustomerSingleton = CustomerSingleton.Instance;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
